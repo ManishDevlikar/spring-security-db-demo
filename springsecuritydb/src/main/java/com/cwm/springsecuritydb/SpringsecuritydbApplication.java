df79a2b39d9b701668a47a5f982b6a35cf74d92e
ManishDevlikar <manish.devlikar5003@gmail.com>
2024-01-01 20:12:12 +0530

    initial commit

---
+++ springsecuritydb/src/main/java/com/cwm/springsecuritydb/SpringsecuritydbApplication.java
@@ -0,0 +1,13 @@
+package com.cwm.springsecuritydb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecuritydbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecuritydbApplication.class, args);
+	}
+
+}
---
+++ springsecuritydb/src/test/java/com/cwm/springsecuritydb/SpringsecuritydbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cwm.springsecuritydb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecuritydbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
