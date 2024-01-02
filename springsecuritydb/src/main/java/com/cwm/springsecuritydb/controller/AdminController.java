a7bcbe1a09c018487cdb6037ab09c26836c8ab82
ManishDevlikar <manish.devlikar5003@gmail.com>
2024-01-02 14:20:35 +0530

    added security configuration and controllers

---
+++ springsecuritydb/src/main/java/com/cwm/springsecuritydb/controller/AdminController.java
@@ -0,0 +1,13 @@
+package com.cwm.springsecuritydb.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/admin")
+public class AdminController {
+
+}
